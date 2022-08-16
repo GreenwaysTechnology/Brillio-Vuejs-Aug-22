3d3d97177c91fef3af3004206f776e7f50a6b7e5
Subramanian Murugan <sasubramanian_md@hotmail.com>
2022-08-16 12:24:14 +0530

    vue getting started

---
+++ vue-legacy/src/app.js
@@ -0,0 +1,6 @@
+
+//Get create App function from Vue Object by destructrucing
+const { createApp } = Vue
+//create Vue Application Object which represents vue app
+const app = createApp()
+console.log(app)
